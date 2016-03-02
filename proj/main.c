88101be3ec5351c8672e0ece8c0e3cbcae94261b
simon_w9@hotmail.com <simon_w9@hotmail.com>
2016-03-02 22:07:35 +0100

    commented & splash screen

--- proj/main.c
+++ proj/main.c
@@ -3,8 +3,14 @@
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "mipslab.h"  /* Declatations for these labs */
 
+#define ENEMYSHIP 'H'
+#define SPACESHIP '>'
+#define EMPTY ' '
+
 volatile int* portE = (int *)0xbf886110;
 volatile int* trisE = (int *)0xbf886100;
+
+
 
 int moved = 0;
 int timeoutcount = 0;
@@ -12,7 +18,6 @@ int enemyupdate = 0;
 int enemydirection = 1;
 char string[4][16];
 
-int flag = 0;
 
 void enemymovement(void);
 void dostuff(void);
@@ -72,35 +77,49 @@ void init( void )
 int main(void) {
 
 	init(); //init board
-	display_string(0, "Welcome to");
-	display_string(1, "mips invaders");
-	display_string(2, "this screen is");
-	display_string(3, "small :(");
+	//starting screen
+	display_string(0, "   Welcome to");
+	display_string(1, " MIPS INVADERS!");
+	display_string(2, " press button 2");
+	display_string(3, "    to start");
 	display_update();
-	delay(1000);
-	display_string(0, "press button");
-	display_string(1, "2 to start");
-	display_string(2, "");
-	display_string(3, "");
-	display_update();
-	
+	//wait for button 2 press (start)
 	while(!(getbtns() & 0x1)){}
 	
+	//starting messages
+	display_string(0, "turn screen with");
+	display_string(1, "<--the left side");
+	display_string(2, "<----towards you");
+	display_string(3, "<---------------");
+	display_update();
+	delay(1000);
+	display_string(3, "       3");
+	display_update();
+	delay(1000);
+	display_string(3, "       2");
+	display_update();
+	delay(1000);
+	display_string(3, "       1");
+	display_update();
+	delay(1000);
+	
 	int i, j;
+	//place enemies
 	for(i = 0; i < 3; i++){
 		for(j = 14; j> 8; j--){
-			string[i][j] = 'H';
+			string[i][j] = ENEMYSHIP;
 		}
 	}
-	
-	//init enemy position
-	int f, g;
-	for(g = 0; g < 4; g++){
-		for(f = 0; f < 16; f++){
-			if(string[g][f] != 'H') string[g][f] = ' ';
+
+	//make empty spaces ' ' (space)
+	for(i = 0; i < 4; i++){
+		for(j = 0; j < 16; j++){
+			if(string[i][j] != ENEMYSHIP) string[i][j] = EMPTY;
 		}
 	}
-	string[3][0] = '>';
+	//place player
+	string[3][0] = SPACESHIP;
+	//display starting field
 	display_string(0, string[0]);
 	display_string(1, string[1]);
 	display_string(2, string[2]);
@@ -120,12 +139,12 @@ void user_isr( void )
   return;
 }
 
-
+//get switch input
 int getsw(void){
 	int ret = PORTD >> 8;
 	return ret &= 0xf;
 }
-
+//get button input
 int getbtns(void){
 	int ret = PORTD >> 5;
 	return ret &= 0x7;
@@ -139,6 +158,7 @@ void dostuff( void )
 		TMR2 = 0;
 		if(timeoutcount < 9){
 			timeoutcount++;
+			//if button pressed and not already moved
 			if(getbtns() && !moved){
 				move();
 				moved = 1;
@@ -164,12 +184,13 @@ void dostuff( void )
 				}
 			  }*/
 		}else{
-			//print on screen once a second
+			//if button pressed and not already moved
 			if(getbtns() && !moved){
 				move();
 				moved = 1;
 			}
 			timeoutcount = 0;
+			//update enemy movement every other second
 			if(enemyupdate == 1){
 				enemyupdate = 0;
 				enemymovement();
@@ -181,9 +202,9 @@ void dostuff( void )
 			}else{
 				enemyupdate++;
 			}
+			//print on screen once a second
 			display_update();
 			moved = 0;
-
 			*portE += 1;
 		}
 	}
@@ -191,37 +212,46 @@ void dostuff( void )
 
 int wall;
 
+//player movement
 void move(void){
 	int i;
+	//button 4 (left)
 	if(getbtns() == 4){
-		if(string[0][0] == '>'){
+		//if already at edge
+		if(string[0][0] == SPACESHIP){
 		}else{
+			//move player
 			for(i = 0; i < 3; i++){
 			string[i][0] = string[i+1][0];
 			}
-			string[3][0] = ' ';
+			string[3][0] = EMPTY;
 		}
-		
+	//button 3 (right)	
 	}else if(getbtns() == 2){
-		if(string[3][0] == '>'){
+		//if already at edge
+		if(string[3][0] == SPACESHIP){
 		}else{
+			//move player
 			for(i = 3; i > 0; i--){
 			string[i][0] = string[i-1][0];
 			}
-			string[0][0] = ' ';
+			string[0][0] = EMPTY;
 		}
 	} 
 }
 
 void enemymovement(void){
 
-			int qw, we, i, j;
+	int qw, we, i, j;
+	//if moving right
 	if(enemydirection == 1){
+		//check if next to wall
 		for(qw = 14; qw > 0; qw--){
-			if(string[3][qw] == 'H'){
+			if(string[3][qw] == ENEMYSHIP){
 				wall = 1;
 			}
 		}
+		//if at wall move down and change direction
 		if(wall){
 			for(i = 1; i < 15; i++){
 				for(j = 0; j < 4; j++){
@@ -231,20 +261,23 @@ void enemymovement(void){
 			wall = 0;
 			enemydirection = -1;
 		}else{
+			//move all enemies right
 			for(i = 1; i < 15; i++){
 				for(j = 3; j > 0; j--){
 					string[j][i] = string[j-1][i];
 				}
-				string[0][i] = ' ';
+				string[0][i] = EMPTY;
 			}
 		}
-		//left
+	//if moving left
 	}else{
+		//check if next to wall
 		for(qw = 14; qw > 0; qw--){
-			if(string[0][qw] == 'H'){
+			if(string[0][qw] == ENEMYSHIP){
 				wall = 1;
 			}
 		}
+		//if at wall move down and change direction
 		if(wall){
 			for(i = 1; i < 15; i++){
 				for(j = 0; j < 4; j++){
@@ -254,16 +287,15 @@ void enemymovement(void){
 			}
 			wall = 0;
 			enemydirection = 1;
+			
 		}else{
+			//move all enemies left
 			for(i = 1; i < 15; i++){
 				for(j = 0; j < 4; j++){
 					string[j][i] = string[j+1][i];
 				}
-				string[3][i] = ' ';
+				string[3][i] = EMPTY;
 			}
 		}
 	}
 }
-
-
-
